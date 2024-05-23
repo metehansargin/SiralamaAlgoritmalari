f2077fa39341a8998fbd4e711a6cee39f7db217a
metehansargin <msargin1986@gmail.com>
2024-05-23 20:12:59 +0300

    update

--- InsertionSort/main.java
+++ InsertionSort/main.java
@@ -1,9 +1,7 @@
-package MergeSort;
-
+package InsertionSort;
 import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
-        long baslangicZamani = System.currentTimeMillis();
         Scanner sc = new Scanner(System.in);
         System.out.print("Dizinin boyutunu girin: ");
         int n = sc.nextInt();
@@ -12,18 +10,28 @@ public class main {
         for (int i = 0; i < n; i++) {
             x[i] = sc.nextInt();
         }
-        
-        mergesort ms = new mergesort();
-        ms.run(x, n);
+        sc.close();
+
+        insertionSort(x);
 
         System.out.println("Sıralanmış dizi:");
         for (int i = 0; i < n; i++) {
             System.out.print(x[i] + " ");
         }
-    
-        long bitisZamani = System.currentTimeMillis();
-        long calismaSuresi = bitisZamani - baslangicZamani;
-        System.out.println("Kodun çalışma süresi: " + calismaSuresi + " milisaniye.");
     }
-    
+
+    public static void insertionSort(int[] array) {
+        int n = array.length;
+        for (int i = 1; i < n; ++i) {
+            int key = array[i];
+            int j = i - 1;
+
+            // key'den büyük olan öğeleri bir konum öteye taşır
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
+    }
 }
